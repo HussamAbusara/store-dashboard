8a588883f2d6a80f3c1fe2fc30673aef91802773
HussamAbusara <hussamfisalfadul@gmail.com>
2026-04-20 16:27:47 +0300

    Delete features directory

--- features/lists/logic/lists_provider.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../data/lists_service.dart';
-import '../model/list_model.dart';
-
-final listsServiceProvider = Provider((ref) => ListsService());
-
-final listsStreamProvider = StreamProvider<List<ListModel>>((ref) {
-  return ref.watch(listsServiceProvider).getUserLists();
-});
